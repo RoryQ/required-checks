2af659d4cd08edc86f8bd1d8a0d2d69f6bf9a139
Rory Quinn <quinn.rory@gmail.com>
2025-07-29 21:22:08 +1000

    No issue or PR number for merge_group so skip ListFiles

--- pkg/pullrequest/client.go
+++ pkg/pullrequest/client.go
@@ -2,6 +2,7 @@ package pullrequest
 
 import (
 	"context"
+	"database/sql"
 	"errors"
 	"strings"
 
@@ -12,10 +13,14 @@ import (
 type Client struct {
 	Owner  string
 	Repo   string
-	Number int
+	Number Option[int]
 	ctx    *githubactions.GitHubContext
 	gh     *github.Client
 }
+
+type Option[T any] = sql.Null[T]
+
+func Optional[T any](t T) Option[T] { return Option[T]{V: t, Valid: true} }
 
 func (pr Client) ListChecks(ctx context.Context, sha string, options *github.ListCheckRunsOptions) ([]*github.CheckRun, error) {
 	var checks []*github.CheckRun
@@ -41,9 +46,12 @@ func (pr Client) ListChecks(ctx context.Context, sha string, options *github.Lis
 }
 
 func (pr Client) ListFiles(ctx context.Context, options *github.ListOptions) ([]*github.CommitFile, error) {
+	if !pr.Number.Valid {
+		return nil, nil
+	}
 	var files []*github.CommitFile
 	for {
-		filesPage, resp, err := pr.gh.PullRequests.ListFiles(ctx, pr.Owner, pr.Repo, pr.Number, options)
+		filesPage, resp, err := pr.gh.PullRequests.ListFiles(ctx, pr.Owner, pr.Repo, pr.Number.V, options)
 		if err != nil {
 			return nil, err
 		}
@@ -67,11 +75,19 @@ func NewClient(action *githubactions.Action, gh *github.Client) (Client, error) 
 		return Client{}, err
 	}
 
-	owner, repo := getRepo(action, ctx.Event)
-	number, err := getPRNumber(ctx.Event)
-	if err != nil {
-		return Client{}, err
+	var number Option[int]
+	switch ctx.EventName {
+	case "merge_group":
+		action.Debugf("Skipping path globs for merge_group")
+	default:
+		n, err := getPRNumber(ctx.Event)
+		if err != nil {
+			return Client{}, err
+		}
+		number = Optional(n)
 	}
+
+	owner, repo := getRepo(action, ctx.Event)
 	action.Debugf("action context: %s %s", owner, repo)
 	return Client{
 		Owner:  owner,
